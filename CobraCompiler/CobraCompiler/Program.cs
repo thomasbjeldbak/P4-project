b7f5fc3d3cfd70cc5b7280a92a60a661ab712e46
Christian Povlsen <chriskp63@gmail.com>
2023-04-04 14:27:01 +0200

    Quality of life update

--- CobraCompiler/CobraCompiler/Program.cs
+++ CobraCompiler/CobraCompiler/Program.cs
@@ -7,37 +7,40 @@ namespace CobraCompiler
     {
         static void Main(string[] args)
         {
-            while (true)
+            var exprText = File.ReadAllText("..\\..\\..\\code.txt");
+
+            if (string.IsNullOrWhiteSpace(exprText))
             {
                 Console.Write("> ");
-                var exprText = Console.ReadLine();
-
-                if (string.IsNullOrWhiteSpace(exprText))
-                    break;
-
-                var inputStream = new AntlrInputStream(new StringReader(exprText));
-                var lexer = new ExprLexer(inputStream);
-                var tokenStream = new CommonTokenStream(lexer);
-                var parser = new ExprParser(tokenStream);
-
-                //try
-                //{
-                var cst = parser.program(); //CFG -> parse tree 
-                var ast = new BuildASTVisitor().VisitProgram(cst); //parse tree -> AST
-                var st = new SymbolTable().BuildSymbolTable(ast); //AST -> Symbol Table
-                new TypeChecker(st).Visit((ProgramNode)ast); //AST, Symbol Table -> TypeChecker
-                Console.WriteLine("DONE!");
-                //var value = new EvaluateExpressionVisitor().Visit(ast);
-
-                //Console.WriteLine("= {0}", value);
-                //}
-                //catch (Exception ex)
-                //{
-                //    Console.WriteLine(ex.Message);
-                //}
-
-                //Console.WriteLine();
+                exprText = Console.ReadLine();
             }
+
+            if (string.IsNullOrWhiteSpace(exprText))
+                return;
+
+            var inputStream = new AntlrInputStream(new StringReader(exprText));
+            var lexer = new ExprLexer(inputStream);
+            var tokenStream = new CommonTokenStream(lexer);
+            var parser = new ExprParser(tokenStream);
+
+            //try
+            //{
+            var cst = parser.program(); //CFG -> parse tree 
+            var ast = new BuildASTVisitor().VisitProgram(cst); //parse tree -> AST
+            var st = new SymbolTable().BuildSymbolTable(ast); //AST -> Symbol Table
+            new TypeChecker(st).Visit((ProgramNode)ast); //AST, Symbol Table -> TypeChecker
+            Console.WriteLine("DONE!");
+
+            //var value = new EvaluateExpressionVisitor().Visit(ast);
+
+            //Console.WriteLine("= {0}", value);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            //Console.WriteLine();
         }
     }
 }
