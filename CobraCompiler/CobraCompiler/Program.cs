ab121986eeb49821a4cb7a325865e19d0d775342
Christian Povlsen <chriskp63@gmail.com>
2023-04-26 14:22:33 +0200

    Fixed symboltable error :)

--- CobraCompiler/CobraCompiler/Program.cs
+++ CobraCompiler/CobraCompiler/Program.cs
@@ -23,53 +23,53 @@ namespace CobraCompiler
             if (string.IsNullOrWhiteSpace(exprText))
                     return;
 
-                var inputStream = new AntlrInputStream(new StringReader(exprText));
-                var lexer = new ExprLexer(inputStream);
-                var tokenStream = new CommonTokenStream(lexer);
-                var parser = new ExprParser(tokenStream);
+            var inputStream = new AntlrInputStream(new StringReader(exprText));
+            var lexer = new ExprLexer(inputStream);
+            var tokenStream = new CommonTokenStream(lexer);
+            var parser = new ExprParser(tokenStream);
+            var cst = parser.program();
 
-                var errorHandler = new ErrorHandler();
-                parser.RemoveErrorListeners(); // remove the default ConsoleErrorListener
-                parser.AddErrorListener(errorHandler); // set your ErrorHandler as the error listener'
+            var errorHandler = new ErrorHandler();
+            parser.RemoveErrorListeners(); // remove the default ConsoleErrorListener
+            parser.AddErrorListener(errorHandler); // set your ErrorHandler as the error listener'
 
-                var cst = parser.program();
-                if (errorHandler.SyntaxErrorMessages.Count > 0)
+            if (errorHandler.SyntaxErrorMessages.Count > 0)
+            {
+                Console.WriteLine("Syntax errors:");
+                foreach (var errorMessage in errorHandler.SyntaxErrorMessages)
                 {
-                    Console.WriteLine("Syntax errors:");
-                    foreach (var errorMessage in errorHandler.SyntaxErrorMessages)
-                    {
-                        Console.WriteLine(errorMessage);
-                    }
-                    Environment.Exit(1);
+                    Console.WriteLine(errorMessage);
                 }
+                Environment.Exit(1);
+            }
 
-                var ast = new BuildASTVisitor().VisitProgram(cst);
-                var st = new SymbolTable(errorHandler).BuildSymbolTable(ast);
-                if (errorHandler.SymbolErrorMessages.Count > 0)
+            var ast = new BuildASTVisitor().VisitProgram(cst);
+            var st = new SymbolTable(errorHandler).BuildSymbolTable(ast);
+            if (errorHandler.SymbolErrorMessages.Count > 0)
+            {
+                Console.WriteLine("Symbol errors:");
+                foreach (var errorMessage in errorHandler.SymbolErrorMessages)
                 {
-                    Console.WriteLine("Symbol errors:");
-                    foreach (var errorMessage in errorHandler.SymbolErrorMessages)
-                    {
-                        Console.WriteLine(errorMessage);
-                    }
+                    Console.WriteLine(errorMessage);
                 }
-                new TypeChecker(st, errorHandler).Visit((ProgramNode)ast);
-                if (errorHandler.TypeErrorMessages.Count > 0)
+            }
+            new TypeChecker(st, errorHandler).Visit((ProgramNode)ast);
+            if (errorHandler.TypeErrorMessages.Count > 0)
+            {
+                Console.WriteLine("Type errors:");
+                foreach (var errorMessage in errorHandler.TypeErrorMessages)
                 {
-                    Console.WriteLine("Type errors:");
-                    foreach (var errorMessage in errorHandler.TypeErrorMessages)
-                    {
-                        Console.WriteLine(errorMessage);
-                    }
+                    Console.WriteLine(errorMessage);
                 }
+            }
 
-                #region CodeGeneration
-                var sb = new StringBuilder();
-                sb = new Emitter(sb, st).Visit((ProgramNode)ast);
+            #region CodeGeneration
+            var sb = new StringBuilder();
+            sb = new Emitter(sb, st).Visit((ProgramNode)ast);
 
-                File.WriteAllText("../GeneratedProgram.txt", sb.ToString());
+            File.WriteAllText("../GeneratedProgram.txt", sb.ToString());
 
-                #endregion
+            #endregion
 
             Console.WriteLine("DONE!");
         
