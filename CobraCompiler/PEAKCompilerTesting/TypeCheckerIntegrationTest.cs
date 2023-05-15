2ea1acbedea66a8a244af597e45b7e84c93ce0e6
barth010may <borup.rasmus@gmail.com>
2023-05-15 13:40:33 +0200

    more tests

--- CobraCompiler/PEAKCompilerTesting/TypeCheckerIntegrationTest.cs
+++ CobraCompiler/PEAKCompilerTesting/TypeCheckerIntegrationTest.cs
@@ -55,35 +55,6 @@ public class TypeCheckerIntegrationTest
         That(VisitMethodCalled(blockNode.Commands[2], symbolTable), Is.True);
         That(VisitMethodCalled(blockNode.Commands[3], symbolTable), Is.True);
     }
-    
-    [Test]
-    public void Visit_DeclarationNode_UpdatesSymbolAndChecksExpression()
-    {
-        // Arrange
-        var symbolTable = new SymbolTable(new ErrorHandler());
-        var declarationNode = new ASTNodes.DeclarationNode
-        {
-            Identifier = new ASTNodes.IdentifierNode { Name = "variable", TypeNode = new ASTNodes.NumberNode()},
-            Expression = new ASTNodes.NumberNode() { Value = 10 }
-        };
-        RunCodeFromFileTestHelper("var variable = 10;");
-        var programNode = new ASTNodes.ProgramNode()
-        {
-            Commands = new List<ASTNodes.CommandNode>
-            {
-                declarationNode
-            }
-        };
-        var scope = new Scope() { Block = programNode };
-        symbolTable._stackScopes.Push(scope);
-
-        // Act
-        symbolTable.Visit(declarationNode);
-
-        // Assert
-        // Assert that the symbol's expression is updated correctly
-        Assert.AreEqual(10, ((ASTNodes.DeclarationNode)symbolTable.Lookup("variable", programNode)?.Reference!)?.Expression.Value);
-    }
 
     private bool VisitMethodCalled(ASTNodes.CommandNode command, SymbolTable symbolTable)
     {
